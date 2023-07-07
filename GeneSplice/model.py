3e1294fe654cdd33f21fe5008cb98527b65ed2dd
Kye <kye@apacmediasolutions.com>
2023-07-07 12:51:37 -0400

    clean up

--- GeneSplice/model.py
+++ GeneSplice/model.py
@@ -3,7 +3,7 @@ import torch
 from torchscale.architecture.decoder import DecoderConfig
 from torchscale.architecture.decoder import Decoder
 from torchscale.component.embedding import PositionalEmbedding
-from transformers AutoTokenizer
+from transformers import AutoTokenizer
 
 
 from torch.nn import Embedding, Module
