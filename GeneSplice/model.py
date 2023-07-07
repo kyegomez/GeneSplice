21b838f0aa4127aa36e02029772ad657b1d51ff8
Kye <kye@apacmediasolutions.com>
2023-07-07 11:00:25 -0400

    gene splice tokenizer

--- GeneSplice/model.py
+++ GeneSplice/model.py
@@ -11,36 +11,17 @@ import bitsandbytes
 
 class GeneSpliceTokenizer:
     def __init__(self):
-        self.processor = CLIPProcessor.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K")
         self.tokenizer = AutoTokenizer.from_pretrained(
             "EleutherAI/gpt-neox-20b",
-            additional_special_tokens=["<image>", "</image>"],
             eos_token="<eos>",
             pad_token="<pad>",
             extra_ids=0,
             model_max_length=8192
         )
-        self.im_idx, self.im_end_idx = self.tokenizer.convert_tokens_to_ids(["<image>", "</image>"])
 
     def tokenize_texts(self, texts):
-        texts = self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True).input_ids
-        image_tokens = torch.tensor([[self.im_idx, self.im_end_idx]] * texts.shape[0])
-        return torch.cat([texts[:, 0:1], image_tokens, texts[:, 1:]], dim=1), texts
-
-    def tokenize_images(self, images):
-        return self.processor(images=images, return_tensors="pt").pixel_values
-
-    def tokenize(self, sample):
-        text_tokens, only_text_tokens = self.tokenize_texts(sample["target_text"])
-        attention_mask = text_tokens != self.tokenizer.pad_token_id
-        dummy_image_features = torch.ones((text_tokens.shape[0], 64))
-        attention_mask = torch.cat([dummy_image_features, attention_mask], dim=1)
-        return {
-            "text_tokens": text_tokens,
-            "images": self.tokenize_images(sample["image"]),
-            "labels": only_text_tokens,
-            "attention_mask": attention_mask,
-        }
+        return self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True).input_ids
+    
     
 
 class GeneSplice(Module):
