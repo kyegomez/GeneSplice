eef735d94d9f9275f0e5a990ae462ba0e0553f7e
Kye <kye@apacmediasolutions.com>
2023-07-07 13:11:04 -0400

    circular import

--- GeneSplice/__init__.py
+++ GeneSplice/__init__.py
@@ -1,4 +1,4 @@
-from GeneSplice.model import GeneSpliceTokenizer, GeneSplice
+# from GeneSplice.model import GeneSpliceTokenizer, GeneSplice
 from GeneSplice.training import Train
 from torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
 from torchscale.torchscale.component.embedding import PositionalEmbedding
